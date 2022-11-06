1ba707d61e032a5359626117e8384bdc21512dc4
nipalang <lukanipalang@gmail.com>
2022-11-06 22:18:31 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,71 @@
+// let name, age, isgood, width
+
+// name = 'John paul'
+// age = 90
+// isgood = false
+// width = 56.9
+// console.log(name, age, isgood, width)
+
+// age = '45'
+  
+// casting
+
+// console.log(parseInt(parseFloat(age)+45.6))
+// console.log(toString(age))
+
+// post increment
+
+
+// let age = 56
+// age++
+// console.log(age)
+
+
+// let age = 40
+// console.log(age+=50)
+ 
+// let food = ['rice', 'beans', 'milk', 'cake']
+//  food[0] = 'chicken'
+//  food.push('beef')
+// console.log(food)
+
+let about = {
+    name: 'Rita Jonah',
+    age: 45,
+    isFemale: true
+    
+}
+
+console.log((about.name))
+
+const a = 10
+const b = 50
+
+const sum = a + b
+console.log(sum)
+
+// age = 3
+// let access_pin = 5539
+
+// if (age >= 5 & access_pin ==5639){
+//     console.log('you are not welcome')
+// }
+// else if (age <= 5 && access_pin != 555){
+//     console.log('go and buy the pin')
+// }
+// else{
+//     console.log('welcome to the party')
+// }
+
+let cars = [
+    {
+        color: 'blue',
+        type: 'toyota'
+    },
+    {
+        color: 'indigo',
+        type: 'honda'
+    }
+]
+
+console.log(cars[1].type)
