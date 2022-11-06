673868152d046113cc4f8c12c50d99215192866c
Jonathan Felicity <jonathanfelicity@mail.com>
2022-11-06 23:35:47 +0100

    first commit

---
+++ agropages/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AgropagesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'agropages'
---
+++ agropages/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=150)
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
